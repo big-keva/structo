75b34af5f670d02ed0c88a3b27eaa634b721d2b1
Keva <keva@rambler.ru>
2026-03-23 11:12:44 +0300

    newly added

---
+++ bounds.hpp
@@ -0,0 +1,17 @@
+# if !defined( __structo_bounds_hxx__ )
+# define __structo_bounds_hxx__
+# include <cstdint>
+
+namespace structo
+{
+  struct Bounds
+  {
+    uint32_t  uLower;
+    uint32_t  uUpper;
+
+    Bounds(): uLower( 0 ), uUpper( -1 ) { }
+    Bounds( uint32_t min, uint32_t max ): uLower( min ), uUpper( max ) { }
+  };
+}
+
+# endif   // !defined( __structo_bounds_hxx__ )
